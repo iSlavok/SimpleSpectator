da095f822888b467c17eb6ae84a46c950f1afbe0
iSlavok <slavaterehin28@gmail.com>
2024-09-14 23:36:14 +0500

    bugfix

--- src/main/kotlin/online/slavok/simpleSpectator/Command.kt
+++ src/main/kotlin/online/slavok/simpleSpectator/Command.kt
@@ -9,20 +9,20 @@ import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 
 class Command : CommandExecutor {
-    private val positions = mutableMapOf<Player, Location>()
+    private val positions = mutableMapOf<String, Location>()
     override fun onCommand(sender: CommandSender, command: Command, label: String, agrs: Array<out String>?): Boolean {
         if (sender !is Player) {
             sender.sendMessage("Команду может использовать только игрок")
             return true
         }
-        if (positions.contains(sender)) {
-            sender.teleport(positions[sender]!!)
+        if (positions.contains(sender.name)) {
+            sender.teleport(positions[sender.name]!!)
             sender.gameMode = GameMode.SURVIVAL
-            positions.remove(sender)
+            positions.remove(sender.name)
             sender.sendMessage(MiniMessage.miniMessage().deserialize("<gray>Felarmonia</gray> <white><bold>></bold></white> <blue>Вы вышли из режима наблюдателя.</blue>"))
             return true
         } else {
-            positions[sender] = sender.location
+            positions[sender.name] = sender.location
             sender.gameMode = GameMode.SPECTATOR
             sender.sendMessage(MiniMessage.miniMessage().deserialize("<gray>Felarmonia</gray> <white><bold>></bold></white> <aqua>Вы вошли в режим наблюдателя.</aqua>"))
             return true
